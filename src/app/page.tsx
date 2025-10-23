7a487e9afc6f07fcc7c7a408921ce1b392cffc71
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2025-10-23 16:15:10 +0300

    [UPDATE] added packages section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Destinations from "./components/home/routes/Destinations";
 import Landing from "./components/home/Landing/Landing";
+import Packages from "./components/home/packages/Packages";
 export default function Home() {
   return (
     <div className="homepage">
       <Landing />
       <Destinations />
+      <Packages/>
       </div>
   );
 }
